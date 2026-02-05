df0f48938b3786af6d044499f4436fe08b365606
Huyen Trang <oydurorktlnuaiavlcnryf@ns.do.edu.vn>
2026-02-05 14:05:02 +0700

    Delete server.bat

--- server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting local server for Product Dashboard...
-echo.
-echo Server running at: http://localhost:8000
-echo Press Ctrl+C to stop the server
-echo.
-start "" "http://localhost:8000"
-python -m http.server 8000
